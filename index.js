6cc08bbb1c2b7ff3271fddf8d18f50645b0a6498
semih-yildiz <semihyildiz9553@gmail.com>
2020-10-10 03:19:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 
-
 //Post datasını parse etme
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
